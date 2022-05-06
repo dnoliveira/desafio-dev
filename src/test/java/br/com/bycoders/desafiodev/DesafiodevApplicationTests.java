c74369b109984195887d3ccea4230a9c94960ce5
Douglas Oliveira <douglas.dno@gmail.com>
2022-05-06 08:21:11 -0300

    Init Project

---
+++ src/test/java/br/com/bycoders/desafiodev/DesafiodevApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bycoders.desafiodev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiodevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
